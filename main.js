49a5c8f01a47cc062f5faf8001b43f07fd2c16c3
dijiflex <dijiflex@gmail.com>
2020-12-26 20:36:45 +0300

    Added file logging

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const slash = require('slash'); 
+const log = require('electron-log')
 //Set ENV
-process.env.NODE_ENV = 'development';  
+process.env.NODE_ENV = 'production';  
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
@@ -66,7 +67,7 @@ async function shrinkImage({ imgPath, quality, dest}) {
           
         mainWindow.webContents.send('image:done')
     } catch (error) {
-        console.log(error);
+        log.error(error)
     }
 }
 
